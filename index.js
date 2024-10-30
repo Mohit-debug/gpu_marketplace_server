c2dcd33042d6a3c377427afa8193f0b1a27e214b
Mohit-debug <71378674+Mohit-debug@users.noreply.github.com>
2024-10-30 12:49:49 +0530

    Update index.js
    const cors = require('cors');

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import gpuRoutes from './routes/gpu.js';
 dotenv.config();
 
 const app = express();
+const cors = require('cors');
+
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
