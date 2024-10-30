b3071d36ca1538d064198cc35303088f1faf537f
Mohit-debug <71378674+Mohit-debug@users.noreply.github.com>
2024-10-30 13:00:29 +0530

    Update index.js
    const corsOptions = {
      origin: 'https://gpu-marketplace-frontend.vercel.app', // Update with your frontend URL
      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
      credentials: true, // Enable if you need to send cookies or authorization headers
    };
    
    app.use(cors(corsOptions));

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ import gpuRoutes from './routes/gpu.js';
 dotenv.config();
 
 const app = express();
-const cors = require('cors');
+const corsOptions = {
+  origin: 'https://gpu-marketplace-frontend.vercel.app', // Update with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Enable if you need to send cookies or authorization headers
+};
 
-app.use(cors());
+app.use(cors(corsOptions)); 
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
